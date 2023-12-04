bb4e7586239443d7e999871713aa178aab267626
noahsmiths <90880131+noahsmiths@users.noreply.github.com>
2023-12-04 02:34:23 -0500

    Setup nextjs base

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next';
+import {Providers} from "./providers";
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Instant IDE'
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className='dark'>
+      <body className={inter.className}>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import {Button} from '@nextui-org/button';
+
+export default function Home() {
+  return (
+    <main className="">
+      <div>
+        <Button>Click me</Button>
+      </div>
+    </main>
+  )
+}
